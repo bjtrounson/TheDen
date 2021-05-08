34912a9ee1eb25594089f9cfa54879c420d9bba7
Benjamin Trounson <44112581+bjtrounson@users.noreply.github.com>
2021-05-08 17:13:32 +1200

    Delete frontend directory

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    images: {
-        domains: ['clips-media-assets2.twitch.tv', `${process.env.DOMAIN}`],
-    },
-    env: {
-        oAuth: "Bearer a8nxcedqlg67j5wx10vuwwk2w635vl",
-        clientId: "it4fn8hx1160kty3mod5tofsxptnvd",
-    },
-}
